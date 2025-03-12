c8fa4c67ca9b57c8e538fbf3d3c0424c852d55b2
sizundiang <rockeln@163.com>
2025-03-12 18:35:24 +0800

    feat: 引入应用打包机制并更新应用图标

--- src/main/java/com/szd/app/recursivecurve/Application.java
+++ src/main/java/com/szd/app/recursivecurve/Application.java
@@ -2,9 +2,11 @@ package com.szd.app.recursivecurve;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Application extends javafx.application.Application {
 
@@ -12,6 +14,8 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-view.fxml"));
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/snowflake.png")));
+        stage.getIcons().add(icon);
         Scene scene = new Scene(fxmlLoader.load(), 500, 440);
         stage.setTitle("Recursive Curve");
         stage.setResizable(false);
